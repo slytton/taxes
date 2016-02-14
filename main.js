e64dd82bf296d58feaad41859581a27c0a669d2e
Seth Lytton <slytton@gmail.com>
2016-02-13 17:44:50 -0700

    complete the taxes exercise

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 module.exports = {
-  taxes: function(){
-    
+  taxes: function(sale){
+    var percentages = [0.1, 0.07, 0.05, 0.03];
+    var taxes = 0;
+    for (var i = 0; sale > 0 && i < percentages.length; i++) {
+      if(sale <= 10 || i === (percentages.length - 1)){
+        taxes += sale * percentages[i];
+      }else{
+        taxes += 10 * percentages[i];
+      }
+      sale -= 10;
+    }
+    return taxes;
   }
 }
--- test/test.js
+++ test/test.js
@@ -3,8 +3,25 @@ var expect = require("chai").expect
 
 
 describe('Taxes', function(){
-  it('should tax the first $10 at 10%');
-  it('should tax the second $10 at 7%');
-  it('should tax the third $10 at 5%');
-  it('should tax everything else at 3%');
+  it('should not tax a negative sale', function(){
+    expect(code.taxes(-1)).to.equal(0);
+  });
+  it('should tax the first $10 at 10%', function(){
+    expect(code.taxes(1)).to.equal(0.1);
+    expect(code.taxes(10)).to.equal(1);
+  });
+  it('should tax the second $10 at 7%', function(){
+    expect(code.taxes(11)).to.equal((10 * 0.1) + (1 * 0.07))
+    expect(code.taxes(20)).to.equal((10 * 0.1) + (10 * 0.07))
+  });
+  it('should tax the third $10 at 5%', function(){
+    expect(code.taxes(21)).to.equal((10 * 0.1) + (10 * 0.07) + (1 * 0.05));
+    expect(code.taxes(30)).to.equal((10 * 0.1) + (10 * 0.07) + (10 * 0.05));
+  });
+  it('should tax everything else at 3%', function(){
+    expect(code.taxes(31)).to.equal((10 * 0.1) + (10 * 0.07) + (10 * 0.05) + (1 * 0.03));
+    expect(code.taxes(40)).to.equal((10 * 0.1) + (10 * 0.07) + (10 * 0.05) + (10 * 0.03));
+    expect(code.taxes(80)).to.equal((10 * 0.1) + (10 * 0.07) + (10 * 0.05) + (50 * 0.03));
+    expect(code.taxes(1000)).to.equal((10 * 0.1) + (10 * 0.07) + (10 * 0.05) + (970 * 0.03));
+  });
 });
